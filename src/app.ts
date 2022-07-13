773db0257b6e336cc25582981287d059a16ab11a
taufiqraw <waskitaufiq@gmail.com>
2022-07-13 14:55:29 +0700

    fix dist

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,8 @@ import 'dotenv/config';
 const app: express.Application = express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, process.env.NODE_ENV === 'development' ? '..': '', 'dist', 'views'));
-app.use(express.static(path.join(__dirname, process.env.NODE_ENV === 'development' ? '..': '', 'dist', "public")))
+app.set('views', path.join(__dirname, 'dist', 'views'));
+app.use(express.static(path.join(__dirname, 'dist', "public")))
 app.use(express.json())
 
 app.use(express.urlencoded({ extended: true }));
