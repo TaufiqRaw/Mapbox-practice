d67fef4f371975eb0104b12709e3438611f5f1d1
taufiqraw <waskitaufiq@gmail.com>
2022-07-13 15:05:14 +0700

    remove sql migration

--- prisma/migrations/20220712135037_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `marker` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `nama` VARCHAR(20) NOT NULL,
-    `latitude` VARCHAR(20) NOT NULL,
-    `longitude` VARCHAR(20) NOT NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- prisma/migrations/20220713052104_remove_limit/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE `marker` MODIFY `address` VARCHAR(191) NOT NULL,
-    MODIFY `name` VARCHAR(191) NOT NULL;
